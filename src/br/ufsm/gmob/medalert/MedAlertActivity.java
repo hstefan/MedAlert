3e7558afd43cd256131e94e41936375f558332ec
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-06-01 09:37:21 -0300

    Initial commit.

---
+++ src/br/ufsm/gmob/medalert/MedAlertActivity.java
@@ -0,0 +1,13 @@
+package br.ufsm.gmob.medalert;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MedAlertActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
