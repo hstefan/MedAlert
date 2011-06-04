cb8de29f6d5c3141587b2957506715a874001e15
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-06-04 19:31:14 -0300

    Adding PeriodicAlarm ORM mapped class.

--- src/br/ufsm/gmob/medalert/dbdata/PeriodicAlarm.java
+++ src/br/ufsm/gmob/medalert/dbdata/PeriodicAlarm.java
@@ -1,8 +1,42 @@
 package br.ufsm.gmob.medalert.dbdata;
 
+import java.util.Date;
+
+import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "PeriodicAlarm")
 public class PeriodicAlarm {
+	@DatabaseField(generatedId = true)
+	private int id;
+	@DatabaseField
+	private Date alert_interval;
+	@DatabaseField(columnName = "id_alarm")
+	private Alarm alarm;
 	
+	public PeriodicAlarm(Date alert_interval, Alarm alarm) {
+		super();
+		this.alert_interval = alert_interval;
+		this.alarm = alarm;
+	}
+
+	public Date getAlert_interval() {
+		return alert_interval;
+	}
+
+	public void setAlert_interval(Date alert_interval) {
+		this.alert_interval = alert_interval;
+	}
+
+	public Alarm getAlarm() {
+		return alarm;
+	}
+
+	public void setAlarm(Alarm alarm) {
+		this.alarm = alarm;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
