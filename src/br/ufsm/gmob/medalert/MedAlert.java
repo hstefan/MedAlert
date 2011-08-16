ba8d404caa9cc05866f9beff3e52023bdd63491c
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-08-16 16:50:49 -0300

    Removing testing code.

--- src/br/ufsm/gmob/medalert/MedAlert.java
+++ src/br/ufsm/gmob/medalert/MedAlert.java
@@ -1,18 +1,12 @@
 package br.ufsm.gmob.medalert;
 
 import java.io.File;
-import java.sql.SQLException;
-import java.util.List;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import br.ufsm.gmob.medalert.db.MedAlertDbHelper;
-import br.ufsm.gmob.medalert.db.entities.AlarmNote;
-import br.ufsm.gmob.medalert.db.entities.User;
 
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
-import com.j256.ormlite.dao.Dao;
 
 public class MedAlert extends OrmLiteBaseActivity<MedAlertDbHelper> {
     /** Called when the activity is first created. */
@@ -26,31 +20,6 @@ public class MedAlert extends OrmLiteBaseActivity<MedAlertDbHelper> {
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	
-    	User u = new User("Hugo Puhlmann", "23423423", "123");
-    	User v = new User("Hugo Puhlmann", "foofofofo", "123");
-    	MedAlertDbHelper helper = getHelper();
-
-    	try {
-			Dao<User, Integer> userDao = helper.getUserDao();
-			Dao<AlarmNote, Integer> noteDao = helper.getAlarmNoteDao();
-			AlarmNote note = new AlarmNote("Foo");
-			noteDao.create(note);
-			userDao.delete(userDao.queryForAll());
-			userDao.create(u);
-			userDao.create(v);
-			List<User> users = userDao.queryForAll();
-			StringBuilder sb = new StringBuilder();
-			for(User us : users) {
-				sb.append(us.getLogin());
-				sb.append("\n");
-			}
-			AlertDialog al = new AlertDialog.Builder(this).setMessage(sb.toString()).setTitle("Usuarios encontrados").create();
-			al.show();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
         setContentView(R.layout.main);
     }
 }
