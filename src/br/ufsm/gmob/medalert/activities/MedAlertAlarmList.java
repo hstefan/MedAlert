b41464b5a67af75c16500abecea2d105104dec96
Hugo Puhlmann <hugopuhlmann@gmail.com>
2012-01-31 18:07:42 -0200

    Simple layout for development.

--- src/br/ufsm/gmob/medalert/activities/MedAlertAlarmList.java
+++ src/br/ufsm/gmob/medalert/activities/MedAlertAlarmList.java
@@ -1,12 +1,26 @@
 package br.ufsm.gmob.medalert.activities;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import br.ufsm.gmob.medalert.R;
 
-public class MedAlertAlarmList extends ListActivity {
+public class MedAlertAlarmList extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);	
+		setContentView(R.layout.main);
+		
+		initTypeSpinner();
+	}
+
+	private void initTypeSpinner() {
+		Spinner spinner = (Spinner)findViewById(R.id.alarmTypeSP);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, 
+				R.array.SpinnerAlarmTypes, android.R.layout.simple_spinner_dropdown_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
 	}
 }
